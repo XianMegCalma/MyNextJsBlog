978473634c1392ee5ae0714bf22fe0cdaeb2fd60
XianMegCalma <xianmegcalma23@gmail.com>
2024-09-09 20:36:08 +0800

    <FinalFinalNEXTJSBLOG>

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[I like to play guitar and watch anime on my free time. As for physical activities, I often go on a long bike trips on weekends, and follow it up with weight lifting 3 times a week.]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
